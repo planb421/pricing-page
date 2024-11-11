7aeb3b44294413543ed42da36d93a304f6985c11
planb421 <BenefactorEmmanuelChisom@outlook.com>
2024-11-11 13:32:07 +0100

    added code files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function toggleSwitch() {
+    var toggle = document.getElementById("toggle");
+    if (toggle.checked) {
+      console.log("Switch is on!");
+    } else {
+      console.log("Switch is off!");
+    }
+  }
